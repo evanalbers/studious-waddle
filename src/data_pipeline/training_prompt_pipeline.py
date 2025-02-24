c5ebbfacfcedc49ba40dfbf17c8c2d166963ee2e
Evan Albers <ealbers@bowdoin.edu>
2025-02-24 13:56:28 -0500

    removed gen tokens function, done in LLM now

--- src/data_pipeline/training_prompt_pipeline.py
+++ src/data_pipeline/training_prompt_pipeline.py
@@ -7,14 +7,6 @@ class training_prompt_pipline:
     def gather_contexts(self):
         """ gets contexts from The Pile """
 
-    def generate_tokens(self):
-        """ from given contexts, generates some number of tokens
-         
-          TODO
-            - decide what a "token" is in this context
-            - write tests
-            -  """
-
     def write_tokens(self):
         """ writes tokens to prompt data file """
 
