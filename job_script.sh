b927c98647cee1ee19ef114728cd822a68550b4e
Evan Albers <ealbers@bowdoin.edu>
2025-04-10 14:43:37 -0400

    cuda???

--- job_script.sh
+++ job_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:v100-pcie:1
 #SBATCH --job-name=sae_training
 #SBATCH --mem=20GB
 #SBATCH --time=00:05:00 
