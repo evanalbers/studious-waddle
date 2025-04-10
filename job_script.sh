6ac1df6c8aa90a58ab101f0eacc3b1a50ae01960
Evan Albers <ealbers@bowdoin.edu>
2025-04-10 11:20:02 -0400

    ugh

--- job_script.sh
+++ job_script.sh
@@ -8,7 +8,7 @@
 #SBATCH --output=sae_training_%j.out
 #SBATCH --error=sae_training_%j.err
 
-# module load anaconda
+module load anaconda
 
 
 python sae_training_script.py --checkpoint_path=sae_model.pt --time_limit=300
